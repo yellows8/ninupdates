849b303039ad5ce4237227940647ee103e65f620
yellows8 <yellows8@users.noreply.github.com>
2014-12-05 11:30:28 -0500

    Added checkurl_lastmodified.sh. On yls8.mtheall.com, this is now used to scan for TMD changes for the following titles: CVer USA, Old3DS NVer USA, and New3DS NVer USA.

---
+++ checkurl_lastmodified.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+url="$1"
+filepath="$2"
+email="$3"
+msg_filepath="$4"
+msg_append="$5"
+
+curl --head $url | grep Last-Modified > ${filepath}new
+
+if [ $? -ne 0 ]
+then
+	exit 1
+fi
+
+if [ -f $filepath ];
+then
+	cmp -s ${filepath}new ${filepath}
+	if [ $? -eq 1 ]
+	then
+		echo "Change detected, sending notification(s)..."
+		msg="Last-Modified for the following URL changed: $url"
+
+		if [ -n "$msg_append" ]
+		then
+			msg="${msg} ${msg_append}"
+		fi
+
+		if [ -n "$msg_filepath" ]
+		then
+			echo $msg >> $msg_filepath
+		fi
+
+		if [ -n "$email" ]
+		then
+			echo $msg | mail -s "URL monitor script" $email
+		fi
+	fi
+fi
+
+mv ${filepath}new ${filepath}
+
