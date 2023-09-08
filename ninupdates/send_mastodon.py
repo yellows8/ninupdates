304aabd32476fca10be2c9c4535726a0f7e40ff8
yellows8 <yellows8@users.noreply.github.com>
2023-09-08 10:45:21 -0400

    send_mastodon.py/tweet.php: Added notif delete functionality.

--- ninupdates/send_mastodon.py
+++ ninupdates/send_mastodon.py
@@ -7,6 +7,7 @@ from mastodon import Mastodon
 parser = argparse.ArgumentParser(description='Send Fediverse/Mastodon-API-compatible messages.')
 parser.add_argument('msg', help='Notification message text.')
 parser.add_argument('--visibility', nargs='?', dest='visibility', help='String value for the visibility field, otherwise the account default is used.')
+parser.add_argument('--delete', nargs='?', dest='delete', help='If specified, this status id is deleted instead of posting the input message.')
 
 args = parser.parse_args()
 
@@ -15,6 +16,10 @@ visibility = args.visibility
 if visibility is not None and len(visibility)==0:
     visibility = None
 
+delete_id = args.delete
+if delete_id is not None and len(delete_id)==0:
+    delete_id = None
+
 # This creds file should be located in the same directory as this script.
 credpath = os.path.join(os.path.realpath(os.path.dirname(__file__)), 'mastodon_usercred.secret')
 if os.path.isfile(credpath) is False:
@@ -22,5 +27,9 @@ if os.path.isfile(credpath) is False:
     sys.exit(2)
 
 mastodon = Mastodon(access_token = credpath)
-mastodon.status_post(msg, visibility=visibility)
+
+if delete_id is None:
+    mastodon.status_post(msg, visibility=visibility)
+else:
+    mastodon.status_delete(delete_id)
 
--- ninupdates/tweet.php
+++ ninupdates/tweet.php
@@ -15,23 +15,24 @@ $twittercfg_access_token
 $twittercfg_access_token_secret
 */
 
-function sendtweet($msg)
+function tweet_init(&$connection)
 {
 	global $twittercfg_consumer_key, $twittercfg_consumer_secret, $twittercfg_access_token, $twittercfg_access_token_secret;
-
-	$ret = 0;
-
 	if(!isset($twittercfg_consumer_key) || !isset($twittercfg_consumer_secret) || !isset($twittercfg_access_token) || !isset($twittercfg_access_token_secret))
 	{
 		echo "Twitter config is missing.\n";
-		$ret = 1;
+		return 1;
 	}
 
-	if($ret == 0)
-	{
-		$connection = new TwitterOAuth($twittercfg_consumer_key, $twittercfg_consumer_secret, $twittercfg_access_token, $twittercfg_access_token_secret);
-		$connection->setApiVersion('2');
-	}
+	$connection = new TwitterOAuth($twittercfg_consumer_key, $twittercfg_consumer_secret, $twittercfg_access_token, $twittercfg_access_token_secret);
+	$connection->setApiVersion('2');
+
+	return 0;
+}
+
+function sendtweet($msg)
+{
+	$ret = tweet_init($connection);
 
 	if($ret == 0)
 	{
@@ -40,7 +41,27 @@ function sendtweet($msg)
 
 		if($statuscode != 200 && $statuscode != 201)
 		{
-			echo "tweet request failed, got HTTP status-code: $statuscode.\n";
+			echo "sendtweet(): request failed, got HTTP status-code: $statuscode.\n";
+			var_dump($out);
+			$ret = 3;
+		}
+	}
+
+	return $ret;
+}
+
+function tweet_delete($in_id)
+{
+	$ret = tweet_init($connection);
+
+	if($ret == 0)
+	{
+		$out = $connection->delete("tweets/$in_id");
+		$statuscode = $connection->getLastHttpCode();
+
+		if($statuscode != 200 && $statuscode != 201)
+		{
+			echo "tweet_delete(): request failed, got HTTP status-code: $statuscode.\n";
 			var_dump($out);
 			$ret = 3;
 		}
@@ -53,11 +74,18 @@ if($_SERVER['SCRIPT_NAME'] === "tweet.php")
 {
 	if($argc<2)
 	{
-		echo("Usage:\nphp tweet.php <msg>\n");
+		echo("Usage:\nphp tweet.php <msg> [if specified, delete this id instead]\n");
 		exit(1);
 	}
 
-	sendtweet($argv[1]);
+	if($argc==2)
+	{
+		sendtweet($argv[1]);
+	}
+	else
+	{
+		tweet_delete($argv[2]);
+	}
 }
 
 ?>
